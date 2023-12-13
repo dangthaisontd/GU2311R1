58af1d2ab211205c5f48da394cf2fcc666b85566
dangthaisontd <dangthaisontd@gmail.com>
2023-12-13 21:55:09 +0700

    Them lenh Lap

---
+++ Bai3/LoopEach/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    
+    static int[]  mang ={0,1,2,3,4,5,6,7,8,9};
+    private static void Main(string[] args)
+    {
+         foreach (int item in mang)
+         {
+            Console.WriteLine(item);
+         }
+    }
+}
